266f6d9934a8202ee113f5490fb600a83da6e9fa
Sherlyn Mora <sherlynn.mora@gmail.com>
2026-07-25 15:36:01 -0500

    finished lab consecutive substrings

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,24 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.substring(i, j));
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
 }
 
 module.exports = consecutiveSubstrings;
-
-
